73578d367308cd8b29bab7086d429f73546a144a
SatriaEgez <satriaegez@gmail.com>
2023-09-25 17:32:10 +0800

    firts commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// Contoh fungsi JavaScript jika Anda memerlukannya
+// Misalnya, untuk menambahkan perilaku interaktif pada fitur-fitur
+
+// Fungsi untuk menampilkan halaman Dashboard
+function showDashboard() {
+    // Tulis logika untuk menampilkan konten dashboard di sini
+}
+
+// Fungsi untuk menampilkan halaman Raport
+function showRaport() {
+    // Tulis logika untuk menampilkan konten raport di sini
+}
+
+// Fungsi untuk menampilkan halaman Pembayaran
+function showPembayaran() {
+    // Tulis logika untuk menampilkan konten pembayaran di sini
+}
+
+// Fungsi untuk menampilkan halaman Konseling
+function showKonseling() {
+    // Tulis logika untuk menampilkan konten konseling di sini
+}
+
+// Fungsi untuk menampilkan halaman Profil
+function showProfil() {
+    // Tulis logika untuk menampilkan konten profil di sini
+}
+
+// Fungsi untuk menampilkan halaman Keluar
+function showKeluar() {
+    // Tulis logika untuk keluar dari aplikasi di sini
+}
+
+// Contoh penggunaan fungsi pada tautan navigasi
+document.querySelector('nav a[href="#dashboard"]').addEventListener('click', showDashboard);
+document.querySelector('nav a[href="#raport"]').addEventListener('click', showRaport);
+document.querySelector('nav a[href="#pembayaran"]').addEventListener('click', showPembayaran);
+document.querySelector('nav a[href="#konseling"]').addEventListener('click', showKonseling);
+document.querySelector('nav a[href="#profil"]').addEventListener('click', showProfil);
+document.querySelector('nav a[href="#keluar"]').addEventListener('click', showKeluar);
